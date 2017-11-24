195024e283a91d77d7e8d3ed417653f7ac31b0c4
wayne <molockree@gmail.com>
2017-11-24 23:23:46 +0800

    add all assets

---
+++ Assets/Scripts/ImageTurnPage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageTurnPage : MonoBehaviour {
+
+	public Sprite[] spritesArrays;
+
+	public Image image;
+
+	public Text pageNumText;
+
+
+	private int curent_page = 0;
+
+	void Start() {
+
+		PrintPageNum();
+
+		RefreshPages(curent_page);
+	}
+
+	private void PrintPageNum () {
+		pageNumText.text = (curent_page + 1).ToString() + "/" + spritesArrays.Length.ToString();
+	}
+
+
+	public void NextPage() {
+
+		if (curent_page < spritesArrays.Length - 1) {
+			curent_page = curent_page + 1;
+
+			RefreshPages(curent_page);
+
+			PrintPageNum();
+		}
+		
+	}
+
+    public void PreviousPage() {
+        if (curent_page > 0) {
+            curent_page = curent_page - 1;
+
+            RefreshPages(curent_page);
+			PrintPageNum();
+        }
+
+    }
+
+	private void RefreshPages(int page) {
+		image.sprite = spritesArrays[page];
+	}
+
+
+}
---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+	public GameObject videoScreen;
+
+	private VideoPlayer videoPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+		videoPlayer = videoScreen.GetComponent<VideoPlayer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Play() {
+		videoPlayer.Play();
+
+    }
+
+    public void Pause() {
+		videoPlayer.Pause();
+    }
+    public void Stop() {
+		videoPlayer.Stop();
+    }
+
+}
